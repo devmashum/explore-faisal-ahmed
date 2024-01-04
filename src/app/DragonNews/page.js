e786281d1756b2198cf8795069e33ee66eac97a5
devmashum <dev.mashum@gmail.com>
2024-01-04 13:41:41 +0100

    The dragon news project details added

--- src/app/DragonNews/page.js
+++ src/app/DragonNews/page.js
@@ -11,44 +11,54 @@ const DragonNews = () => {
 
             <div className='p-5'>
           <div className='flex gap-3'>
-            <div><h3 className=' text-slate-900 text-xl lg:text-2xl'>Projekttitel: </h3></div>
+            <div><h3 className=' text-slate-900 text-xl lg:text-2xl'>Projektname: </h3></div>
             <div><h2 className="text-slate-900 text-xl lg:text-2xl font-bold ">
             The Dragon News</h2>
             </div>
             </div>
-            <h3 className="text-slate-900 text-base font-bold mt-3">Projektdetails:</h3>
+            <h3 className="text-slate-900 text-xl underline font-bold mt-3">Projektdetails:</h3>
             <h4 className="text-slate-900 my-3 text-justify text-base">
-            Ich befinde mich derzeit im Prozess des Lernens von Next.js und Material-UI, und als Teil dieser Erkundung habe ich ein Online-Zeitungsportal mit Next.js und Material-UI entwickelt, wobei MongoDB als Datenbank dient. <br />
-<br/>
-<b>Hauptmerkmale:</b>
+            Das Online-Zeitungsportal mit Next.js und Material-UI entwickelt, wobei MongoDB als Datenbank dient, ist eine Online-Plattform, welche aktuelle Nachrichten zeigt. Die Benutzer können kostenlos Nachrichten lesen und sich z.B. über Politik oder Sport informieren. <br />
+            <br />
+<b className='text-xl underline'>Hauptmerkmale:</b>
 <br />
 <br />
 <div>
 <b>Artikelanzeige:</b>
-<br />
-Präsentation von Nachrichtenartikeln mit Funktionen wie Überschriften, Bildern und Zusammenfassungen.
-<br />
-<b>Kategorienavigation:</b> <br />
+<ul className='list-disc ml-10'>
+  <li>
+  Präsentation von Nachrichtenartikeln mit Funktionen wie Überschriften, Bildern und Zusammenfassungen
+  </li>
+</ul>
+<b>Kategorienavigation:</b> 
+<ul className='list-disc ml-10'>
+  <li>
+  Organisation von Nachrichtenartikeln in Kategorien für einfache Navigation und Benutzerfreundlichkeit
+  </li>
+</ul>
+<b>Responsive Design:</b> 
+<ul className='list-disc ml-10'>
+  <li>
+  Gewährleistung einer nahtlosen Benutzererfahrung auf verschiedenen Geräten, einschließlich Desktops, Tablets und Smartphones
+  </li>
+</ul>
+<b>Dynamisches Routing:</b>
+<ul className='list-disc ml-10'>
+  <li>
+  Nutzung von Next.js dynamischem Routing für effiziente Bearbeitung unterschiedlicher Seiten und Inhalte
+  </li>
+</ul>
 
-Organisation von Nachrichtenartikeln in Kategorien für einfache Navigation und Benutzerfreundlichkeit.
-<br />
-<b>Responsive Design:</b> <br />
-Gewährleistung einer nahtlosen Benutzererfahrung auf verschiedenen Geräten, einschließlich Desktops, Tablets und Smartphones. <br />
-<b>Dynamisches Routing:</b> <br />
-Nutzung von Next.js dynamischem Routing für effiziente Bearbeitung unterschiedlicher Seiten und Inhalte. <br />
-<b>Neueste Nachrichten-Sektion:</b> Präsentation der neuesten und trendigen Nachrichten prominent auf der Startseite.
-<br />
-
-<b>Benutzerauthentifizierung:</b>
-Firebase wird für die Benutzerregistrierung sowie die An- und Abmeldungs-Funktionalität verwendet.
-<br />
-<b>Sicherheit:</b> Die Website ist durch die Verwendung von JSON Web Token (JWT) abgesichert, um eine sichere Benutzererfahrung zu gewährleisten.
-<br />
-<b>Datenspeicher:</b>
-MongoDB wird genutzt, um alle relevanten Daten zu speichern und eine effiziente Datenverwaltung sicherzustellen.
+<b>Datenbank:</b>
+<ul className='list-disc ml-10'>
+  <li>
+  MongoDB wird genutzt, um alle relevanten Daten zu speichern und eine effiziente Datenverwaltung sicherzustellen
+  </li>
+</ul>
 </div>
 
             </h4>
+            <h4 className='font-bold mb-5'>Technologien, die ich für diese Plattform verwendet habe:</h4>
             <div className="grid lg:grid-cols-3 grid-cols-2 gap-5 text-center">
             <h2 className='border border-xl border-black'>JavaScript</h2>
             <h2 className='border border-xl border-black'>Next.js</h2>
